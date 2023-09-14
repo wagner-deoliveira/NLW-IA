7e40f26d23b59878f2c9b96867621b4c46c3d2af
Wagner Rosa <60220411+wagner-deoliveira@users.noreply.github.com>
2023-09-15 01:42:03 +0200

    Finishing touches

---
+++ upload-ia-server/prisma/seed.ts
@@ -0,0 +1,70 @@
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.prompt.deleteMany()
+
+    await prisma.prompt.create({
+        data: {
+            title: 'YouTube title',
+            template: `Your role is to generate three titles for a YouTube video.
+
+Below, you will receive a transcription of this video; use this transcription to generate the titles.
+Below, you will also receive a list of titles; use this list as a reference for the titles to be generated.
+
+The titles must have a maximum of 60 characters.
+The titles should be catchy and attractive to maximize clicks.
+
+Return ONLY the three titles in list format as shown below:
+'''
+- Title 1
+- Title 2
+- Title 3
+'''
+'''
+
+Transcription:
+'''
+{transcription}
+'''`.trim()
+        }
+    })
+
+    await prisma.prompt.create({
+        data: {
+            title: 'YouTube description',
+            template: `Your role is to generate a concise description for a YouTube video.
+
+Below, you will receive a transcription of this video, use this transcription to create the description.
+
+The description should have a maximum of 80 words in the first person and should include the main points of the video.
+
+Use compelling words that capture the reader's attention.
+
+In addition, at the end of the description, include a list of 3 to 10 hashtags in lowercase containing keywords from the video.
+
+The return should follow the following format:
+'''
+Description.
+
+#hashtag1 #hashtag2 #hashtag3 ...
+'''
+
+Transcription:
+'''
+{transcription}
+'''`.trim()
+        }
+    })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
--- upload-ia-server/src/routes/generate-ia-completion.ts
+++ upload-ia-server/src/routes/generate-ia-completion.ts
@@ -2,16 +2,17 @@ import {FastifyInstance} from "fastify";
 import {z} from "zod";
 import {prisma} from "../lib/prisma";
 import {openai} from "../lib/openai";
+import {OpenAIStream, streamToResponse} from "ai";
 
 export async function generateIaCompletionRoute(app: FastifyInstance) {
     app.post('/ai/complete', async (req, res) => {
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5)
         })
 
-        const {videoId, template, temperature} = bodySchema.parse(req.body)
+        const {videoId, prompt, temperature} = bodySchema.parse(req.body)
 
         const video = await prisma.video.findUniqueOrThrow({
             where: {
@@ -23,13 +24,20 @@ export async function generateIaCompletionRoute(app: FastifyInstance) {
             return res.status(400).send({error: 'Video transcription wasn\'t generated yet'})
         }
 
-        const promptMessage = template.replace('{transcription}', video.transcription)
+        const promptMessage = prompt.replace('{transcription}', video.transcription)
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo-16k',
             temperature,
-            messages: [{role: 'user', content: promptMessage}]
+            messages: [{role: 'user', content: promptMessage}],
+            stream: true
         })
 
-        return response
+        const stream = OpenAIStream(response)
+        streamToResponse(stream, res.raw, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST'
+            }
+        })
     })
 }
